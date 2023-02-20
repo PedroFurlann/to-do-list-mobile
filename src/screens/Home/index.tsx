d6cf62bbbe1d6cd479f8ef920c141ad40f4b3739
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-20 15:02:13 -0300

    Create Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#333333',
+    flex: 1,
+    padding: 24,
+  },
+
+  
+})
